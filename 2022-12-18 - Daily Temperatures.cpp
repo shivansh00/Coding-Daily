875445d69db8b6ec65187aa0ac9af5c79a93d95e
Shivansh Thapliyal <73345015+shivansh00@users.noreply.github.com>
2022-12-18 10:31:22 +0530

    739. Daily Temperatures

---
+++ 2022-12-18 - Daily Temperatures.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    vector<int> dailyTemperatures(vector<int>& tp) {
+        vector<int> result(tp.size());
+        stack<vector<int>> s;
+        for (int i= 0; i < tp.size(); ++i) {
+            while (!s.empty() and tp[i] > s.top()[0]) {
+                result[s.top()[1]]= i- s.top()[1];
+                s.pop();
+            }
+            s.push({tp[i], i});
+        }
+        return result;
+    }
+};200

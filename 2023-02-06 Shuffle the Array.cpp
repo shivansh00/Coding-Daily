3c77e5e313ff0884e65baa2585eb03b717000e5e
Shivansh Thapliyal <73345015+shivansh00@users.noreply.github.com>
2023-02-06 09:29:47 +0530

    1470. Shuffle the Array

---
+++ 2023-02-06 Shuffle the Array.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    vector<int> shuffle(vector<int>& nums, int n) {
+        vector<int> ret;
+        for (int i= 0, j= n; i < n; ++i, ++j) {
+            ret.push_back(nums[i]);
+            ret.push_back(nums[j]);
+        }
+        return ret;
+    }
+};

6e9f74367e1161079c6dacd8b51eed0d43009e03
Shivansh Thapliyal <73345015+shivansh00@users.noreply.github.com>
2023-06-14 21:25:07 +0530

    26. Remove Duplicates from Sorted Array.cpp

--- 2022-11-11 Remove Duplicates from Sorted Array.cpp
+++ 2022-11-11 Remove Duplicates from Sorted Array.cpp
@@ -3,12 +3,10 @@ public:
     int removeDuplicates(vector<int>& nums) {
         int cur= nums[0];
         int k= 1;
-        for (int i= 1; i < nums.size(); i++) {
+        for (int i= 1; i < nums.size(); i++)
             if (nums[i] == cur)
                 nums[i]= INT_MAX;
-            else
-                cur= nums[i], k++;
-        }
+            else    cur= nums[i], k++;
         sort(nums.begin(), nums.end());
         return k;
     }
